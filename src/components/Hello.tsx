3153382fe9a2fa92e181c6859d082b822c053d12
Pablo.Orozco <pablo.orozco@gorillalogic.com>
2018-09-02 15:24:43 -0600

    Change entry-point

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -9,24 +9,20 @@ export interface Props {
 }
 
 /**
- * Class based component approach.
+ * Stateless function component (an SFC).
  */
-class Hello extends React.Component<Props, object> {
-  render() {
-    const { name, enthusiasmLevel = 1 } = this.props;
-
-    if (enthusiasmLevel <= 0) {
-      throw new Error('You could be a little more enthusiastic. :D');
-    }
-
-    return (
-      <div className="hello">
-        <div className="greeting">
-          Hello {name + getExclamationMarks(enthusiasmLevel)}
-        </div>
-      </div>
-    );
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
   }
+
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+    </div>
+  );
 }
 
 export default Hello;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import Hello from './components/Hello';
+
 /** 
- * Entry-point
-*/
+ * Entry-point.
+ */
 ReactDOM.render(
-  <App />,
+  <Hello name="TypeScript" enthusiasmLevel={10} />,
   document.getElementById('root') as HTMLElement
 );
 
